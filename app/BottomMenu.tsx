17a5bc7f7d0b28840106c8506acf815651cd731a
Filip Altankov <filipaltankov@hotmail.com>
2024-03-21 18:22:31 -0400

    Early project skeleton

---
+++ app/BottomMenu.tsx
@@ -0,0 +1,3 @@
+export const BottomMenu = () => {
+  return <div>BottomMenu</div>;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import { TopMenu } from "./TopMenu";
+import { BottomMenu } from "./BottomMenu";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <TopMenu />
+      <h1>Welcome to Filip's Portofolio!</h1>
+      <h2>Projects</h2>
+      <h2>Languages</h2>
+      <h2>About me</h2>
+      <h2>Contact me</h2>
+      <BottomMenu />
+    </main>
+  );
 }
