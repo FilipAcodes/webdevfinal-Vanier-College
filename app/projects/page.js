25b6f6a2e1d7a9f57b5bc6de269b033b26644769
Filip Altankov <filipaltankov@hotmail.com>
2024-03-29 16:31:57 -0400

    carousel implementation wip

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,9 +1,44 @@
+"use client";
 import "./projectpage.css";
 import { BottomMenu } from "../BottomMenu/BottomMenu";
+import Carousel from "react-bootstrap/Carousel";
+// import ExampleCarouselImage from "components/ExampleCarouselImage";
+
+import "./projectpage.css";
 export default function Projects() {
   return (
     <>
       <BottomMenu />
+      <div className="carouselcontainer">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpFN1Tvo80rYwu-eXsDNNzsuPITOdtyRPlYIsIqKaIbw&s"
+              text="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img text="Second slide" />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img text="Third slide" />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </>
   );
 }
