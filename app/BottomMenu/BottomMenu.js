06836efdb719b801fdc2e4cf477d10b7c83191f6
Filip Altankov <filipaltankov@hotmail.com>
2024-03-28 20:17:50 -0400

    added links towards pages

--- app/BottomMenu/BottomMenu.js
+++ app/BottomMenu/BottomMenu.js
@@ -5,15 +5,31 @@ import { Email } from "./Email";
 import { FaceBook } from "./FaceBook";
 import { GitHub } from "./GitHub";
 import { LinkedIn } from "./LinkedIn";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const BottomMenu = () => {
+  const pathname = usePathname();
   return (
     <div className="bottomMenu">
-      <TwitterLogo />
-      <FaceBook />
-      <GitHub />
-      <LinkedIn />
-      <Email />
+      <div className="routers">
+        <Link href="/" className={`link${pathname === "/" ? "active" : ""}`}>
+          Home
+        </Link>
+        <Link
+          href="/projects"
+          className={`link${pathname === "/projects" ? "active" : ""}`}
+        >
+          Projects
+        </Link>
+      </div>
+      <div className="contactflex">
+        <TwitterLogo />
+        <FaceBook />
+        <GitHub />
+        <LinkedIn />
+        <Email />
+      </div>
     </div>
   );
 };
---
+++ app/projects/page.js
@@ -0,0 +1,9 @@
+import { BottomMenu } from "../BottomMenu/BottomMenu";
+
+export default function Projects() {
+  return (
+    <div>
+      <BottomMenu />
+    </div>
+  );
+}
