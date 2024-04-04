bf7a0d60f0ec9b8ddb02347030a4e7042174dc5a
Filip Altankov <filipaltankov@hotmail.com>
2024-04-04 17:22:07 -0400

    services/blog underconstruction notice

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,8 +1,15 @@
 import { BottomMenu } from "../BottomMenu/BottomMenu";
+import underConstruction from "../underconstruction.jpg";
+import "../blog/underconstruction.css";
 export default function BlogPage() {
   return (
     <>
       <BottomMenu />
+      <img
+        src={underConstruction.src}
+        alt="under construction"
+        className="underconstruction"
+      />
     </>
   );
 }
--- app/services/page.js
+++ app/services/page.js
@@ -1,9 +1,15 @@
 import { BottomMenu } from "../BottomMenu/BottomMenu";
+import underConstruction from "../underconstruction.jpg";
 
 export default function ServicesPage() {
   return (
     <>
       <BottomMenu />
+      <img
+        src={underConstruction.src}
+        alt="under construction"
+        className="underconstruction"
+      />
     </>
   );
 }
